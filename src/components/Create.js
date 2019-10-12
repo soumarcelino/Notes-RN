1a9f1ac8345da3b9bba74f38fac0d3543644e0f1
soumarcelino <marcelinomatiasdesouza@gmail.com>
2019-10-12 17:37:07 -0300

    Add back button

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
-const Create = () => {
+const Create = ({ navigation: { navigate } }) => {
     return (
         <View>
             <Text>NOVA TELA</Text>
+            <Button
+                title="Voltar"
+                onPress={() => { navigate('Home') }}
+            />
         </View>
     );
 };
